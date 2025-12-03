e446dcdb4f58abb74513edec08f337e5cb855af6
Ferozaffs <feroza_ffs@hotmail.com>
2025-12-03 22:23:12 +0100

    Completed day 3 part 1

--- 2025/cmd/day3/day3.go
+++ 2025/cmd/day3/day3.go
@@ -1,9 +1,13 @@
 package day3
 
 import (
-	"2025/cmd"
+	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
+
+	"2025/cmd"
 
 	"github.com/spf13/cobra"
 )
@@ -30,5 +34,107 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
-	return 0, 0
+	banks := [][]int{}
+
+	scanner := bufio.NewScanner(strings.NewReader(data))
+	for scanner.Scan() {
+		bankStr := string(scanner.Bytes())
+
+		values := []int{}
+
+		for _, r := range bankStr {
+			values = append(values, int(r-'0'))
+		}
+
+		banks = append(banks, values)
+	}
+
+	ans1 := 0
+	ans2 := 0
+
+	for _, bank := range banks {
+
+		batteryPair := map[int]int{}
+		batteryArray := map[int]int{}
+
+		for compare := 9; compare >= 0; compare-- {
+
+			lowestPairIndex := -1
+			seekValue := 10
+			for idx, value := range batteryPair {
+				if lowestPairIndex == -1 {
+					lowestPairIndex = idx
+				} else if idx < lowestPairIndex || value < seekValue {
+					lowestPairIndex = idx
+					seekValue = value
+				}
+			}
+
+			lowestArrayIndex := -1
+			seekValue = 10
+			for idx, value := range batteryArray {
+				if lowestArrayIndex == -1 {
+					lowestArrayIndex = idx
+				} else if idx < lowestArrayIndex || value < seekValue {
+					lowestArrayIndex = idx
+					seekValue = value
+				}
+			}
+
+			for index := len(bank) - 1; index >= 0; index-- {
+				value := bank[index]
+				if value == compare {
+					if len(batteryPair) < 2 && (lowestPairIndex > len(bank)-3-len(batteryPair) || index > lowestPairIndex) {
+						_, ok := batteryPair[index]
+						if ok == false {
+							batteryPair[index] = value
+						}
+					}
+					if len(batteryArray) < 12 && (lowestArrayIndex > len(bank)-13-len(batteryArray) || index > lowestArrayIndex) {
+						_, ok := batteryArray[index]
+						if ok == false {
+							batteryArray[index] = value
+						}
+					}
+				}
+			}
+		}
+
+		index0 := -1
+		index1 := -1
+		for idx := range batteryPair {
+			if index0 != -1 {
+				index1 = idx
+			} else {
+				index0 = idx
+			}
+		}
+
+		if index0 < index1 {
+			ans1 += batteryPair[index0]*10 + batteryPair[index1]
+		} else {
+			ans1 += batteryPair[index1]*10 + batteryPair[index0]
+		}
+
+		index := make([]int, 0, len(batteryArray))
+		for idx := range batteryArray {
+			index = append(index, idx)
+		}
+
+		sort.Ints(index)
+
+		value := 0
+		count := 1
+		for i := len(index) - 1; i >= 0; i-- {
+			idx := index[i]
+
+			value += batteryArray[idx] * count
+
+			count *= 10
+		}
+
+		ans2 += value
+	}
+
+	return ans1, ans2
 }
--- 2025/cmd/day3/day3_test.go
+++ 2025/cmd/day3/day3_test.go
@@ -4,10 +4,17 @@ import (
 	"testing"
 )
 
-func TestSample1(t *testing.T) {
+func TestSample(t *testing.T) {
+	sampleData := `987654321111111
+811111111111119
+234234234234278
+818181911112111`
 
-}
-
-func TestSample2(t *testing.T) {
-
+	ans1, ans2 := Solve(sampleData)
+	if ans1 != 357 {
+		t.Fatalf("Mismatch! Expected 357 got %d", ans1)
+	}
+	if ans2 != 3121910778619 {
+		t.Fatalf("Mismatch! Expected 3121910778619 got %d", ans2)
+	}
 }
