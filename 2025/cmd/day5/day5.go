7e835155e4ae22d491239db1853b10592697a782
Ferozaffs <feroza_ffs@hotmail.com>
2025-12-07 20:10:17 +0100

    Completed day 5

--- 2025/cmd/day5/day5.go
+++ 2025/cmd/day5/day5.go
@@ -1,12 +1,22 @@
 package day5
 
 import (
-	"2025/cmd"
+	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
+
+	"2025/cmd"
 
 	"github.com/spf13/cobra"
 )
+
+type Interval struct {
+	start int
+	end   int
+}
 
 var DayCmd = &cobra.Command{
 	Use:   "day5",
@@ -30,5 +40,91 @@ func Run1() {
 }
 
 func Solve(data string) (int, int) {
-	return 0, 0
+	intervals := []Interval{}
+	ingredients := []int{}
+
+	parseIngredients := false
+	scanner := bufio.NewScanner(strings.NewReader(data))
+	for scanner.Scan() {
+		row := string(scanner.Bytes())
+		if len(row) == 0 {
+			parseIngredients = true
+		} else if parseIngredients {
+			ingredient, _ := strconv.Atoi(row)
+			ingredients = append(ingredients, ingredient)
+		} else {
+			index := strings.Split(row, "-")
+			s, _ := strconv.Atoi(index[0])
+			e, _ := strconv.Atoi(index[1])
+
+			interval := Interval{start: s, end: e}
+			intervals = append(intervals, interval)
+		}
+	}
+
+	merged := true
+	for merged {
+		merged, intervals = MergeIntervals(intervals)
+	}
+
+	ans1 := CheckFresh(ingredients, intervals)
+
+	ans2 := 0
+	for _, interval := range intervals {
+		delta := interval.end - interval.start + 1
+		ans2 += delta
+	}
+
+	return ans1, ans2
+}
+
+func MergeIntervals(intervals []Interval) (bool, []Interval) {
+	merged := false
+
+	i := 0
+	for i < len(intervals) {
+		j := i + 1
+		for j < len(intervals) {
+			base := intervals[i]
+			other := intervals[j]
+
+			merge := false
+			if other.start >= base.start && other.start <= base.end {
+				merge = true
+			} else if other.end >= base.start && other.end <= base.end {
+				merge = true
+			} else if other.start <= base.start && other.end >= base.end {
+				merge = true
+			}
+
+			if merge {
+				merged = true
+
+				intervals[i].start = int(math.Min(float64(base.start), float64(other.start)))
+				intervals[i].end = int(math.Max(float64(base.end), float64(other.end)))
+
+				intervals = append(intervals[:j], intervals[j+1:]...)
+			} else {
+				j++
+			}
+		}
+		i++
+	}
+
+	return merged, intervals
+}
+
+func CheckFresh(ingredients []int, intervals []Interval) int {
+	fresh := 0
+
+	for _, ingredient := range ingredients {
+		for _, interval := range intervals {
+			if ingredient >= interval.start && ingredient <= interval.end {
+				fresh++
+				break
+			}
+		}
+	}
+
+	return fresh
 }
--- 2025/cmd/day5/day5_test.go
+++ 2025/cmd/day5/day5_test.go
@@ -4,10 +4,24 @@ import (
 	"testing"
 )
 
-func TestSample1(t *testing.T) {
+func TestSample(t *testing.T) {
+	sampleData := `3-5
+10-14
+16-20
+12-18
 
-}
+1
+5
+8
+11
+17
+32`
 
-func TestSample2(t *testing.T) {
-
+	ans1, ans2 := Solve(sampleData)
+	if ans1 != 3 {
+		t.Fatalf("Mismatch! Expected 3 got %d", ans1)
+	}
+	if ans2 != 14 {
+		t.Fatalf("Mismatch! Expected 14 got %d", ans2)
+	}
 }
